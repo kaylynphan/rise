61989f30b8935d0a6836e1e838df1d59ed209bcd
Kaylyn Phan <kaylynphan@fb.com>
2022-07-07 13:44:51 -0700

    incomplete translation of PoseNetInput

--- rise/PoseNet/PoseNetInput.h
+++ rise/PoseNet/PoseNetInput.h
@@ -11,7 +11,21 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface PoseNetInput : MLFeatureProvider
+@interface PoseNetInput : NSObject <MLFeatureProvider>
+
+@property (nonatomic) struct CGImage *imageFeature;
+@property (nonatomic) struct CGSize *imageFeatureSize;
+@property (strong, nonatomic) NSMutableSet *featureNames;
+
+- (id) init:(struct CGImage *)image withSize:(struct CGSize *)size;
+
+- (MLFeatureValue * _Nullable) featureValueForName:(NSString *)featureName;
+
+
+
+
+
+
 
 
 
--- rise/PoseNet/PoseNetInput.m
+++ rise/PoseNet/PoseNetInput.m
@@ -9,4 +9,33 @@
 
 @implementation PoseNetInput
 
+static NSString *imageFeatureName = @"image";
+
+- (id) init:(struct CGImage *)image withSize:(struct CGSize *)size {
+    self.imageFeature = image;
+    self.imageFeatureSize = size;
+    self.featureNames = [[NSMutableSet alloc] init];
+    [self.featureNames addObject: imageFeatureName];
+    return self;
+}
+
+- (MLFeatureValue * _Nullable) featureValueForName:(NSString *)featureName {
+    if (featureName != imageFeatureName) {
+        return nil;
+    }
+    // otherwise
+    NSMutableDictionary *options = [[NSMutableDictionary init] alloc];
+    //VNImageCropAndScaleOptionScaleFill = 2
+    [options setObject:@(2) forKey:MLFeatureValueImageOptionCropAndScale];
+    
+    MLFeatureValue *result = [MLFeatureValue featureValueWithCGImage:self.imageFeature pixelsWide:self.imageFeatureSize->width pixelsHigh:self.imageFeatureSize->height pixelFormatType:self.imageFeature->pixelFormatInfo.rawValue options:options error:(NSError *__autoreleasing  _Nullable * _Nullable)];
+    if (result) {
+        return result;
+    } else {
+        return nil;
+    }
+    
+}
+
+
 @end
