9756cdd6f09d3aed584b3a7410fbbbab7548b302
Kaylyn Phan <kaylynphan@fb.com>
2022-07-12 15:02:59 -0700

    add WorkoutCellDelegate to pass workout in segue from start button to guide VC

--- rise/ViewControllers/GuideViewController.h
+++ rise/ViewControllers/GuideViewController.h
@@ -6,10 +6,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Workout.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface GuideViewController : UIViewController
+
+@property (weak, nonatomic) Workout *workout;
 
 @end
 
