f59424d5d0e7d809be92bf7cec14a40f22e6731f
Kaylyn Phan <kaylynphan@fb.com>
2022-07-12 15:07:41 -0700

    confirm that workout is being passed in segue to guide VC

--- rise/ViewControllers/GuideViewController.h
+++ rise/ViewControllers/GuideViewController.h
@@ -13,6 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface GuideViewController : UIViewController
 
 @property (weak, nonatomic) Workout *workout;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 
 @end
 
