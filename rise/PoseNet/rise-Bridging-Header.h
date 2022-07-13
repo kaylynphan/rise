37434a7ff6dbc3afb59f62f1badb7002bd391ce8
Kaylyn Phan <kaylynphan@fb.com>
2022-07-13 14:43:25 -0700

    attempt again, fail

--- rise/PoseNet/rise-Bridging-Header.h
+++ rise/PoseNet/rise-Bridging-Header.h
@@ -2,3 +2,10 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+/*
+#import "Joint.swift"
+#import "CGImage+Extension.swift"
+#import "CGPoint+Extension.swift"
+#import "Pose.swift"
+#import "PoseBuilder.swift"
+*/
