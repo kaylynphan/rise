a32c7316492a717cb434e382b18ff14f3f1ae03f
Kaylyn Phan <kaylynphan@fb.com>
2022-07-13 11:29:56 -0700

    posenet-swift

---
+++ rise/PoseNet/Model/rise-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  rise-Bridging-Header.h
+//  rise
+//
+//  Created by Kaylyn Phan on 7/13/22.
+//
+
+#ifndef rise_Bridging_Header_h
+#define rise_Bridging_Header_h
+
+
+#endif /* rise_Bridging_Header_h */
