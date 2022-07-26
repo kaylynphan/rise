74e43cb6f9924edc39e7553b7a49771049b56f0b
Kaylyn Phan <kaylynphan@fb.com>
2022-07-26 15:42:21 -0700

    DatePicker can choose new preferred notification time and update Parse

--- rise/ViewControllers/ProfileViewController.m
+++ rise/ViewControllers/ProfileViewController.m
@@ -8,19 +8,21 @@
 #import "ProfileViewController.h"
 #import "../Models/User.h"
 #import "GalleryViewController.h"
+#import <Parse/Parse.h>
 
 @interface ProfileViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *displayNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *emailLabel;
-@property (weak, nonatomic) IBOutlet UIButton *timeButton;
-- (IBAction)didTapTimeButton:(id)sender;
 - (IBAction)didTapLogout:(id)sender;
 @property (weak, nonatomic) IBOutlet UILabel *scheduleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *profileLabel;
 @property (weak, nonatomic) IBOutlet UILabel *settingsLabel;
 @property (weak, nonatomic) IBOutlet UILabel *riseWillNotifyLabel;
 - (IBAction)didTapBack:(id)sender;
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+- (IBAction)dateChanged:(id)sender;
+
 
 @end
 
@@ -40,12 +42,10 @@
     dateComponents.hour = [user[@"preferredHour"] integerValue];
     dateComponents.minute = [user[@"preferredMinute"] integerValue];
     NSDate *date = [[NSCalendar currentCalendar] dateFromComponents:dateComponents];
-    NSLog(@"%@", [formatter stringFromDate:date]);
-    //self.timeButton.titleLabel.text = [formatter stringFromDate:date]; // not working?
-    self.timeButton.enabled = FALSE;
-    [self.timeButton setTitle:[formatter stringFromDate:date] forState:UIControlStateNormal];
-    self.timeButton.enabled = TRUE;
     
+    [self.datePicker setDate:date];
+    
+    NSLog(@"%@", [formatter stringFromDate:date]);
     
     [self.scheduleLabel sizeToFit];
     
@@ -71,10 +71,23 @@
     }];
 }
 
-- (IBAction)didTapTimeButton:(id)sender {
-}
-
 - (IBAction)didTapBack:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)dateChanged:(id)sender {
+    NSLog(@"Finished editing date");
+    NSDate *newDate = self.datePicker.date;
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:(NSCalendarUnitHour | NSCalendarUnitMinute) fromDate:newDate];
+    User *user = [User currentUser];
+    user[@"preferredHour"] = [NSNumber numberWithInteger:components.hour];
+    user[@"preferredMinute"] = [NSNumber numberWithInteger:components.minute];
+    [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+        if (error != nil) {
+            NSLog(@"Error uploading new preferred notification time: %@", error.localizedDescription);
+        } else {
+            NSLog(@"Successfully updated user's preferred notification time");
+        }
+    }];
 }
 @end
