e1ae40c252d279f4b8f5142c62fb762a8c8af6d8
Kaylyn Phan <kaylynphan@fb.com>
2022-07-28 14:01:46 -0700

    gallery vc header displays user's completion status for today based on Parse data. Text and Check-mark/X-mark updated

--- rise/Views/GalleryViewHeader.m
+++ rise/Views/GalleryViewHeader.m
@@ -12,6 +12,7 @@
 @implementation GalleryViewHeader
 
 static NSString *const kPFUserDisplayName = @"displayName";
+static NSString *const kPFUserCompletionDates = @"completionDates";
 
 - (void)awakeFromNib {
     [super awakeFromNib];
@@ -19,7 +20,7 @@ static NSString *const kPFUserDisplayName = @"displayName";
     [self setupHelloLabel];
     self.myWorkoutsLabel.font = [UIFont fontWithName:@"Poppins-medium" size:30];
     [self.myWorkoutsLabel sizeToFit];
-    [self.todayCompletionLabel sizeToFit];
+    [self setupCompletionLabel];
 }
 
 
@@ -29,6 +30,36 @@ static NSString *const kPFUserDisplayName = @"displayName";
     self.helloLabelBackground.backgroundColor = [UIColor colorWithRed:247.0/255.0f green:248.0/255.0f blue:248.0/255.0f alpha:1.0f];
     self.helloLabelBackground.layer.cornerRadius = 30;
     self.helloLabelBackground.layer.masksToBounds = YES;
+}
+
+- (void)setupCompletionLabel {
+    User *user = [User currentUser];
+    if (user != nil) {
+        // if the completion dates array is empty
+        if (user[kPFUserCompletionDates] == nil) {
+            self.todayCompletionLabel.text = @"You have not stretched today.";
+            [self.xOrCheckMarkImage setImage:[UIImage imageNamed:@"X Mark Component"]];
+        } else if ([user[kPFUserCompletionDates] isKindOfClass:[NSArray class]]) {
+            NSMutableArray *userCompletionDates = user[kPFUserCompletionDates];
+            if ([[userCompletionDates lastObject] isKindOfClass:[NSDate class]]) {
+                NSDate *mostRecentDate = [userCompletionDates lastObject];
+                // if the user already recorded having stretched today
+                if ([[NSCalendar currentCalendar] isDateInToday:mostRecentDate]) {
+                    self.todayCompletionLabel.text = @"You've already stretched today.";
+                    [self.xOrCheckMarkImage setImage:[UIImage imageNamed:@"Check Mark Component"]];
+                } else {
+                    // otherwise the user has not stretched today
+                    self.todayCompletionLabel.text = @"You have not stretched today.";
+                    [self.xOrCheckMarkImage setImage:[UIImage imageNamed:@"X Mark Component"]];
+                }
+            }
+        } else {
+            NSLog(@"There was an issue trying to access user's completion dates from Parse.");
+            self.todayCompletionLabel.text = @"You have not stretched today.";
+            [self.xOrCheckMarkImage setImage:[UIImage imageNamed:@"X Mark Component"]];
+        }
+    }
+    [self.todayCompletionLabel sizeToFit];
 }
  
 
